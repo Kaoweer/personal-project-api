ef03bdc9e56b7271e370910ab69b06b93b7d177d
Kaoweer <kaow.arch@gmail.com>
2025-01-18 00:13:55 +0700

    finalized

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,18 +1,21 @@
-const prisma = require("../config")
+const prisma = require("../config");
 
-module.exports.getUserProfile = async(req,res) => {
-  const {userId} = req.params
-  console.log(userId,"---------------------------------------------------------------------------")
+module.exports.getUserProfile = async (req, res) => {
+  const { userId } = req.params;
+  console.log(
+    userId,
+    "---------------------------------------------------------------------------"
+  );
   const userProfile = await prisma.user.findUnique({
-    where : {
-      id : +userId
-    }
-  })
+    where: {
+      id: parseInt(userId),
+    },
+  });
   const userProgram = await prisma.trainingProgram.findMany({
-    where : {
-      id : +userId,
-      status : "PUBLIC"
-    }
-  })
-  res.json({userProfile,userProgram})
-}
+    where: {
+      id: +userId,
+      status: "PUBLIC",
+    },
+  });
+  res.json({ userProfile, userProgram });
+};
